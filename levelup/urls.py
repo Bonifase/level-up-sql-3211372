49f9704b8d84ada2a62c9009082605a9af172f33
Bonifase <orwabonifase@gmail.com>
2022-12-23 01:41:03 +0300

    add levelup api view

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,12 +1,10 @@
-from django.urls import include, path
-from rest_framework import routers
-from views import restaurant_view
+from django.urls import path
+from .views import restaurant_view
 
-router = routers.DefaultRouter()
-router.register(r'restaurant', restaurant_view)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path(
+      'restaurant',
+      restaurant_view,
+      name="restaurant"
+    ),
 ]
--- levelup/views.py
+++ levelup/views.py
@@ -1,5 +1,55 @@
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import render
+from django.conf import settings
+
+import sqlite3
+import json
+
+def row_to_dict(cursor: sqlite3.Cursor, row: sqlite3.Row) -> dict:
+    data = {}
+    for idx, col in enumerate(cursor.description):
+        data[col[0]] = row[idx]
+    return data
+
+def execute_sql(db, query):
+    connection = sqlite3.connect(db)
+    connection.row_factory = row_to_dict
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        result = json.dumps(result)
+        cursor.close()
+
+    except sqlite3.OperationalError as error:
+        print(error)
+        cursor.close()
+        return error
+
+    return result
+
+
+SOLUTIONS = {
+    'Check Availability': '01_restaurant/solutions/01_01_invitations_so.sql',
+    'Anniversary Invitation': '02_library/solutions/02_02_anniversary_invitation_so.sql',
+}
 
 # Create your views here.
-def restaurant_view(request, *args, **kwargs):
-  return redirect(request.body)
+@api_view(['GET', 'POST'])
+def restaurant_view(request):
+    query_type = request.data.get('query', 'Check Availability')
+    correct_query = request.data.get('user_query',None)
+    result = None
+    path = settings.BASE_DIR / SOLUTIONS.get(query_type)
+
+    # with open(path, 'r') as f:
+    #     correct_query = f.read()
+    print(correct_query)
+    if correct_query:
+        db = settings.BASE_DIR / '01_restaurant/restaurant.db'
+        result = execute_sql(db, correct_query)
+
+    return Response(result)
+
